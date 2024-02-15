9c33da67ba954681a18a58c22a41916e4bd52ecd
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-02-15 15:39:14 -0800

    removed T for StructArray{<:Vortex}

--- src/vortex.jl
+++ src/vortex.jl
@@ -27,7 +27,7 @@ $(TYPEDSIGNATURES)
 
 Returns the strengths of all point vortices in the `StructArray` `vl` as `ScalarData`.
 """
-function getstrengths(vl::StructArray{Vortex})
+function getstrengths(vl::StructArray{<:Vortex})
     return ScalarData(vl.Γ)
 end
 
@@ -36,7 +36,7 @@ $(TYPEDSIGNATURES)
 
 Returns the positions of all point vortices in the `StructArray` `vl` as `VectorData`.
 """
-function getpositions(vl::StructArray{Vortex})
+function getpositions(vl::StructArray{<:Vortex})
     return VectorData(vl.x,vl.y)
 end
 
@@ -45,7 +45,7 @@ $(TYPEDSIGNATURES)
 
 Sets the `x` and `y` fields of the point vortices in the `StructArray` `vl` to the entries of `xnew` and `ynew`, respectively.
 """
-function setpositions!(vl::StructArray{Vortex}, xnew, ynew)
+function setpositions!(vl::StructArray{<:Vortex}, xnew, ynew)
     vl.x .= xnew
     vl.y .= ynew
 end
@@ -55,7 +55,7 @@ $(TYPEDSIGNATURES)
 
 Sets the `Γ` field of the point vortices in the `StructArray` `vl` to the entries of `Γnew`.
 """
-function setstrengths!(vl::StructArray{Vortex}, Γnew, idx=nothing)
+function setstrengths!(vl::StructArray{<:Vortex}, Γnew, idx=nothing)
     if isnothing(idx)
         vl.Γ .= Γnew
     else

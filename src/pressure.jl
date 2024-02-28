8ca1195ce6474a744d4f1697674be49cddf39db7
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-02-27 17:34:17 -0800

    new pressure calculations

--- src/pressure.jl
+++ src/pressure.jl
@@ -16,6 +16,21 @@ function pressurejump!(dp::ScalarData{N},γn::ScalarData{N},γnp1::ScalarData{N}
 
 end
 
+function pressurejump!(dp::ScalarData{N},fn::ScalarData{N},vm1::VortexModel,fnp1::ScalarData{N},v̄s::VectorData{N},Δt::Real,Δs, sys::ImmersedLayers.ILMSystem{<:GridPotentialILMProblem}) where {N}
+    @unpack base_cache, extra_cache = sys
+    @unpack g, nrm, gcurl_cache, sdata_cache = base_cache
+    @unpack CLinvCT, Rn = extra_cache
+
+    for i in 1:length(dp)
+        dp[i] = sum(view(fnp1,1:i))*Δs[i] + sum(vm1.vortices.Γ[end-1:end])
+        dp[i] -= sum(view(fn,1:i))*Δs[i]
+    end
+    dp .*= -cellsize(g)/Δt
+    cross!(sdata_cache,nrm,v̄s)
+    γn = fn./Δs
+    dp .-= sdata_cache∘γn
+end
+
 function velocity!(v̄::Edges{Primal,NX,NY},ψ::Nodes{Dual,NX,NY},sys::ImmersedLayers.ILMSystem) where {NX,NY}
     @unpack base_cache = sys
     @unpack g = base_cache

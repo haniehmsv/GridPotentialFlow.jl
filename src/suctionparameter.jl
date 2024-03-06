280ac8330e8353c449396a9d8ba3820bd1837b8d
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-05 18:31:40 -0800

    plate::Polygon

--- src/suctionparameter.jl
+++ src/suctionparameter.jl
@@ -48,8 +48,8 @@ function (/)(p::f̃Limits,c::Number)
     return f̃Limits(p.min/c, p.max/c)
 end
 
-function _computef̃limit(SP::SuctionParameter, plate::Plate, Γ₀)
-    f̃ = -SP*2π*1/Γ₀
+function _computef̃limit(SP::SuctionParameter, plate::Polygon, Γ₀)
+    f̃ = -SP*2π*plate.len/Γ₀
     return f̃
 end
 
